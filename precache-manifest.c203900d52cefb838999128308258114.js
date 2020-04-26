630452d3870838a6cf5e1991ac19c7e7b99ed04a
awebdes <m.habibulloev.m@gmail.com>
2020-04-26 17:03:25 +0500

    Updates

---
+++ precache-manifest.c203900d52cefb838999128308258114.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b523564a015e30602472e9a6e88bc557",
+    "url": "/kowriki/index.html"
+  },
+  {
+    "revision": "a823e995033486ecfc4a",
+    "url": "/kowriki/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "54b474f44d8cae510249",
+    "url": "/kowriki/static/css/main.964476d2.chunk.css"
+  },
+  {
+    "revision": "a823e995033486ecfc4a",
+    "url": "/kowriki/static/js/2.c4ea0c28.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kowriki/static/js/2.c4ea0c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b474f44d8cae510249",
+    "url": "/kowriki/static/js/main.3a7b476f.chunk.js"
+  },
+  {
+    "revision": "c0245be78f42a72b5710",
+    "url": "/kowriki/static/js/runtime-main.fc89872b.js"
+  },
+  {
+    "revision": "fa8b2b6d29ab80d1ff54d0887aecc61c",
+    "url": "/kowriki/static/media/eva 1.fa8b2b6d.svg"
+  },
+  {
+    "revision": "baf4ba2bba30690decca6a1c6c316182",
+    "url": "/kowriki/static/media/good.baf4ba2b.svg"
+  },
+  {
+    "revision": "bd9d7048c410021903285abd7d6b3104",
+    "url": "/kowriki/static/media/header-img2.bd9d7048.png"
+  },
+  {
+    "revision": "40ca55d05ebb66c6ec1cc649f9a0a9ae",
+    "url": "/kowriki/static/media/invest.40ca55d0.svg"
+  },
+  {
+    "revision": "e0da10775579a4bff4820fb8378ec28a",
+    "url": "/kowriki/static/media/logo.e0da1077.svg"
+  },
+  {
+    "revision": "764d5f89875aafb8766a9f1bb365ab1f",
+    "url": "/kowriki/static/media/telegramm.764d5f89.svg"
+  },
+  {
+    "revision": "93677c5ad85a453f73783cdabc9af501",
+    "url": "/kowriki/static/media/truck.93677c5a.svg"
+  },
+  {
+    "revision": "5762bbb49cef42a332049e1f16d44050",
+    "url": "/kowriki/static/media/viber.5762bbb4.svg"
+  },
+  {
+    "revision": "dc20f10141276885a76f12cd31e5a9fd",
+    "url": "/kowriki/static/media/vk.dc20f101.svg"
+  },
+  {
+    "revision": "b6b5a39d15c1e86ea1f96d02aa1f8025",
+    "url": "/kowriki/static/media/watsapp.b6b5a39d.svg"
+  }
+]);
